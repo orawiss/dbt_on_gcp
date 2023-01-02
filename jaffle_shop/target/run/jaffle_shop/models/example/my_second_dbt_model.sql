cf9ab52ef80b872b190d54e6b772139ee58f8d09
Wissem <kiwan2005@gmail.com>
2023-01-02 18:25:42 +0100

    Add files via upload

--- jaffle_shop/target/run/jaffle_shop/models/example/my_second_dbt_model.sql
+++ jaffle_shop/target/run/jaffle_shop/models/example/my_second_dbt_model.sql
@@ -1,1 +1,10 @@
 
+
+  create or replace view `data-product-analytic-ecommrce`.`dbt`.`my_second_dbt_model`
+  OPTIONS()
+  as -- Use the `ref` function to select from other models
+
+select *
+from `data-product-analytic-ecommrce`.`dbt`.`my_first_dbt_model`
+where id = 1;
+
